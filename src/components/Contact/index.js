49f7ad2df2bf152452f813dc7bdbb16ebee08e98
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-21 23:55:15 -0700

    form almost fully functional

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,27 +2,37 @@
 // import coverImage from "../../assets/cover/cover-image.jpg"
 
 //PUT THIS INDEX.JS FILE IN SRC/ASSETS/COMPONENTS/COMPONENTNAME TO MAKE A NEW REACT COMPONENT FOR THE SECTION 1ST=IMPORT 2ND=FUNCTIONS 3RD=EXPORT ----> DON'T FORGET TO CAPITALIZE THE 'first letter' BEC. IT USES PASCAL CASING TO SIGNIFY THE REACT COMPONENT
-import React from 'react';
+import React, { useState } from 'react';
+
+//1. IMPORT USESTATE FROM REACT 2. DEFINE THE HOOK VIA AN EXPRESSION NEAR THE TOP OF THE FILE 3. CLEAR THE INPUT FIELDS ON THE COMPONENT LOADING VIA SETTING INITIAL STATE TO EMPTY STRINGS
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    //DESTRUCTURE FORMSATE OBJECT INTO ITS NAMED PROPERTIES-NAME-EMAIL& MESSAGE AND MAKE THE NAME PROPERTY A DYNAMIC VARIABLE DETERMINED BY THE FOR
+    const { name, email, message } = formState;
+    function handleChange(e) {
+
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
     return (
         <section>
             <h1 id="contact"> Contact Me!</h1>
             <form id="contact-form">
-                {/* name:
-                email:
-                message text area: */}
+                {/* ADD THE DEFAULT VALUE TO EACH FORM ELEMENT HANDLING FORM DATA TO ASSIGN INITIAL STATE VALUES FOR RECORD IN THE DOM */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    {/*ASSIGN A FUNCTION, HANDLECHANGE, TO THE ADDED ATTRIBUTE ONCHANGE TO <INPUT> ELEMENT FOR NAME*/}
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
